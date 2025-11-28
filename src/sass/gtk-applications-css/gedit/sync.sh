a81242857fea2c8a9b70e627d9d8045c29e55c1d
Azzy <floatingfloatskies@protonmail.com>
2025-11-28 11:21:07 -0400

    Delete src directory

--- src/sass/gtk-applications-css/gedit/sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget https://gitlab.gnome.org/GNOME/gedit/raw/master/gedit/resources/css/gedit.adwaita.css -O Adwaita.css
-
--- src/sass/gtk-applications-css/nautilus/sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget https://gitlab.gnome.org/GNOME/nautilus/raw/42.beta/src/resources/css/Adwaita.css -O Adwaita.css
-
